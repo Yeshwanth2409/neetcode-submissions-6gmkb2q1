9482e98f3694a543e80262d55bbdcd5148c24c32
Yeshwanth Yedla <156608989+Yeshwanth2409@users.noreply.github.com>
2026-04-28 12:50:39 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,11 @@
+variable = 24
+print(type(variable))
+variable = 2.3
+print(type(variable))
+variable = False
+print(type(variable))
+variable = 'yesh'
+print(type(variable))
+variable = []
+print(type(variable))
+
