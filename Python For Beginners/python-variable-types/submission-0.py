0c991e4010527844c62a17d4b56f44096b6bfd5a
Yeshwanth Yedla <156608989+Yeshwanth2409@users.noreply.github.com>
2026-04-28 12:52:00 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+integer_type = 24
+float_type = 3.2
+bool_type = True
+string_type = '21'
+list_type = []
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
