add8ebe669bf3c76f20ce283b3b058e8ee7920ca
Yeshwanth Yedla <156608989+Yeshwanth2409@users.noreply.github.com>
2026-04-28 15:48:48 -0400

    Add: python-type-conversion-input - submission-0

---
+++ Python For Beginners/python-type-conversion-input/submission-0.py
@@ -0,0 +1,18 @@
+def read_integer() -> int:
+    li = int(input())
+    return li
+
+
+def read_float() -> float:
+    lo = float(input())
+    return lo
+
+
+# do not modify below this line
+print(read_integer())
+print(read_integer())
+print(read_integer())
+
+print(read_float())
+print(read_float())
+print(read_float())
